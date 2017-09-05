46138d819805d524432d24cce9134349bf2426c9
Di0niz <moskovchenkody@gmail.com>
2017-09-06 00:08:21 +0300

    Update SkynetRevolution.go
    Прохождение тестов 1,2

--- CLASSIC PUZZLE - MEDIUM/SkynetRevolution.go
+++ CLASSIC PUZZLE - MEDIUM/SkynetRevolution.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "os"
 
 // описываем граф
 type Graph map[int][]int
@@ -13,17 +14,20 @@ func appendLink(G Graph, fromKey int, toKey int) {
 	
 }
 
-func search(G Graph, node int) {
+func search(G Graph, node int) []int {
 
 	queue := []int{node}
 	marked := map[int]bool{node:true}
+	
+	visited :=  []int{}
 
 	for len(queue) > 0 {
 
 		r := queue[0]
 		queue = queue[1:]
 
-		fmt.Println(r)
+        visited = append(visited, r)
+
 
 		for _, n := range G[r] {
 			if !marked[n] {
@@ -33,24 +37,49 @@ func search(G Graph, node int) {
 		}
 	}
 
-
+    return visited
 }
 
+
+
 func main() {
-	
-	G := make(Graph)
 
-
-	appendLink(G, 1, 2)
-	appendLink(G, 3, 1)
-	appendLink(G, 3, 4)
-	appendLink(G, 5, 4)
-	appendLink(G, 5, 9)
-	appendLink(G, 7, 9)
-	
-	fmt.Println(G)
-
-	search(G, 5)
-	
-
+    // N: the total number of nodes in the level, including the gateways
+    // L: the number of links
+    // E: the number of exit gateways
+    var N, L, E int
+    fmt.Scan(&N, &L, &E)
+    
+    G := make(Graph)
+    
+    for i := 0; i < L; i++ {
+        // N1: N1 and N2 defines a link between these nodes
+        var N1, N2 int
+        fmt.Scan(&N1, &N2)
+        
+        appendLink(G, N1, N2)
+        
+    }
+    for i := 0; i < E; i++ {
+        // EI: the index of a gateway node
+        var EI int
+        fmt.Scan(&EI)
+        fmt.Fprintln(os.Stderr, EI)
+        
+    }
+    
+        fmt.Fprintln(os.Stderr, "> ",G)
+    
+    for {
+        // SI: The index of the node on which the Skynet agent is positioned this turn
+        var SI int
+        fmt.Scan(&SI)
+        
+        
+        fmt.Fprintln(os.Stderr, "> ",SI)
+        visited := search(G, SI)
+        
+        // Example: 0 1 are the indices of the nodes you wish to sever the link between
+        fmt.Println(visited[0],visited[1])
+    }
 }
